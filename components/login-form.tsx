6901d24898b7fd92bb664de2e996ce19896e3ad2
Pan Yunyao <timerainy2@gmail.com>
2025-09-25 22:47:19 +0800

    error

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -13,7 +13,6 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 export function LoginForm({
@@ -24,7 +23,6 @@ export function LoginForm({
   const [password, setPassword] = useState("");
   const [error, setError] = useState<string | null>(null);
   const [isLoading, setIsLoading] = useState(false);
-  const router = useRouter();
 
   const handleLogin = async (e: React.FormEvent) => {
     e.preventDefault();
--- components/logout-button.tsx
+++ components/logout-button.tsx
@@ -2,10 +2,9 @@
 
 import { createClient } from "@/lib/supabase/client";
 import { Button } from "@/components/ui/button";
-import { useRouter } from "next/navigation";
+
 
 export function LogoutButton() {
-  const router = useRouter();
 
   const logout = async () => {
     const supabase = createClient();
