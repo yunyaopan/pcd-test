3f7b3fedde2b1c6549c92707483151f98df5978b
Pan Yunyao <timerainy2@gmail.com>
2025-09-25 21:17:06 +0800

    login issue

--- lib/supabase/middleware.ts
+++ lib/supabase/middleware.ts
@@ -51,9 +51,7 @@ export async function updateSession(request: NextRequest) {
     request.nextUrl.pathname !== "/" &&
     !user &&
     !request.nextUrl.pathname.startsWith("/login") &&
-    !request.nextUrl.pathname.startsWith("/auth") &&
-    !request.nextUrl.pathname.startsWith("/templates") &&
-    !request.nextUrl.pathname.startsWith("/projects")
+    !request.nextUrl.pathname.startsWith("/auth")
   ) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone();
